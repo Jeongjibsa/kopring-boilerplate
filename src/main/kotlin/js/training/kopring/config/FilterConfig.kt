9b32a94a6cee3a988afa7b7cf1fcf03316975c9f
js.jung <js.jung@promptech.co.kr>
2023-03-22 17:45:50 +0900

    FilterConfig을 통해 정의된 filter 등록

---
+++ src/main/kotlin/js/training/kopring/config/FilterConfig.kt
@@ -0,0 +1,26 @@
+package js.training.kopring.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import js.training.kopring.config.filter.ExceptionFilter
+import js.training.kopring.config.filter.LogFilter
+import js.training.kopring.config.filter.JwtFilter
+import js.training.kopring.config.security.JwtProvider
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+class FilterConfig(
+    private val jwtTokenProvider: JwtProvider,
+    private val objectMapper: ObjectMapper
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(builder: HttpSecurity) {
+        val tokenFilter = JwtFilter(jwtTokenProvider)
+        val exceptionFilter = ExceptionFilter(objectMapper)
+        val logFilter = LogFilter()
+        builder.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(exceptionFilter, JwtFilter::class.java)
+        builder.addFilterBefore(logFilter, ExceptionFilter::class.java)
+    }
+}
