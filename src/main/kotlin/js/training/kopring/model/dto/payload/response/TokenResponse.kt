9631564939a804b525388737fb33a0a52d1f998d
js.jung <js.jung@promptech.co.kr>
2023-03-23 16:53:06 +0900

    Token response 시 null value handling

--- src/main/kotlin/js/training/kopring/model/dto/payload/response/TokenResponse.kt
+++ src/main/kotlin/js/training/kopring/model/dto/payload/response/TokenResponse.kt
@@ -1,7 +1,12 @@
 package js.training.kopring.model.dto.payload.response
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
 data class TokenResponse(
+
     val accessToken: String,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     val refreshToken: String?
 ) {
     constructor(accessToken: String) : this(accessToken, null)
